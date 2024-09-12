6df467164abe006c794172c3efda3c2b7bb427e2
Maximilien Gilet <maximilien.gilet@externe.maif.fr>
2024-09-12 09:19:25 +0200

    change markdown plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,9 +1,7 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {},
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 }
