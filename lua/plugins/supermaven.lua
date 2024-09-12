70845bbcfdf9ea9e4c73b6745e2331065576ade4
Maximilien Gilet <maximilien.gilet@externe.maif.fr>
2024-09-12 10:51:12 +0200

    disable supermaven if NVIM_SUPERMAVEN_DISABLE is set

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -16,6 +16,9 @@ return {
         log_level = "off", -- set to "off" to disable logging completely
         disable_inline_completion = false, -- disables inline completion for use with cmp
         disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return vim.env.NVIM_SUPERMAVEN_DISABLE
+        end,
       })
     end,
   },
