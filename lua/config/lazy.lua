5e1692a89132bb464e297ca617e5c616b82c5c50
Maximilien Gilet <maximilien.gilet@externe.maif.fr>
2024-10-22 16:09:31 +0200

    enable tohtml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -52,7 +52,7 @@ require("lazy").setup({
         -- "matchparen",
         -- "netrwPlugin",
         "tarPlugin",
-        "tohtml",
+        -- "tohtml",
         "tutor",
         "zipPlugin",
       },
